7494d43cf9d6c4af97c53c27dd6dfeed6a1ab66f
shahmaran0207 <shahmaran0207@naver.com>
2024-10-03 09:04:15 +0900

    get api

--- src/hooks/useGetTopRatedMovies.js
+++ src/hooks/useGetTopRatedMovies.js
@@ -1,9 +1,25 @@
+import {useInfiniteQuery} from "@tanstack/react-query";
+
 const fetchTopRatedMovies=async (page)=>{
     const response=await fetch(`
     https://api.themoviedb.org/3/movie/top_rated?page=${page}`, {
-      headers:{
-          Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
-      }
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjI2MDg0NmQ0OTMxZWM2OWNiYWU3Y2E1MTRhODgzZiIsIm5iZiI6MTcyNzkxMzMzNS4zOTcwNTMsInN1YiI6IjY2ZmM5YjllMjViNjFjMzNkYTU5NDIxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v4gmtFQMOgYUlaEt4Qpr__lSJpvrzixKdtRliyl6tBs'
+        }
     });
     return response.json();
 };
+
+const useGetTopRatedMovies =()=>{
+    return useInfiniteQuery({
+        queryKey:['top-rated-movie'],
+        queryFn:({pageParam})=>{
+            return fetchTopRatedMovies(pageParam)
+        }
+
+        getNextPageParam:(last)=>{
+            if(last.page<)
+        }
+    })
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import App from './App.jsx';
+import './index.css';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root=React.createRoot(document.getElementById('root'));
+const querryClient=new QueryClient();
+
+root.render(
+    <QueryClientProvider client={querryClient}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </QueryClientProvider>
 )
